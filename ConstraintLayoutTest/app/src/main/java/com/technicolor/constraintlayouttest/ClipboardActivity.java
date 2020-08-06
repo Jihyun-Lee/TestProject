915a17325d8d210ed3b951ff77e3fbda0c06281a
Jayce <dontbeshy8888@gmail.com>
2020-08-06 12:22:31 +0900

    add clipboard test in case intent copied

--- ConstraintLayoutTest/app/src/main/java/com/technicolor/constraintlayouttest/ClipboardActivity.java
+++ ConstraintLayoutTest/app/src/main/java/com/technicolor/constraintlayouttest/ClipboardActivity.java
@@ -1,10 +1,14 @@
 package com.technicolor.constraintlayouttest;
 
+import android.content.BroadcastReceiver;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,8 +18,11 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ClipboardActivity extends AppCompatActivity {
+    private static final String TEST_BROADCAST = "com.test.br.INTENT";
     private ClipboardManager clipboard;
     private ClipData myClip;
+    private String TAG = "test";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,19 +59,39 @@ public class ClipboardActivity extends AppCompatActivity {
 
                 ClipData.Item item = clip.getItemAt(0);
                 Log.d("test", "count : "+ count);
-                Toast.makeText(getApplicationContext(), "clipboard text : " + item.getText().toString(), Toast.LENGTH_SHORT).show();
+                //TODO : distinguish which type was copied...
+                if (item.getText() != null)
+                    Toast.makeText(getApplicationContext(), "clipboard text : " + item.getText().toString(), Toast.LENGTH_SHORT).show();
+                else //intent
+                    Toast.makeText(getApplicationContext(), "clipboard text : " + item.getIntent().toString(), Toast.LENGTH_SHORT).show();
+
             }
         });
         btn3.setOnClickListener(new View.OnClickListener(){
+            //set intent to clipboard
             @Override
             public void onClick(View view) {
+                Intent appIntent = new Intent( TEST_BROADCAST );
+                ClipData clip = ClipData.newIntent("Intent", appIntent);
+                clipboard.setPrimaryClip(clip);
 
             }
         });
         btn4.setOnClickListener(new View.OnClickListener(){
+            //get intent
             @Override
             public void onClick(View view) {
-
+                Intent pasteIntent = clipboard.getPrimaryClip().getItemAt(0).getIntent();
+                if( pasteIntent != null){
+                    //handle
+                    Log.d(TAG, "send broadcast");
+                    Toast.makeText(getApplicationContext(), "send broadcast", Toast.LENGTH_SHORT).show();
+                    sendBroadcast(pasteIntent);
+                } else {
+                    //ignore
+                    Log.d(TAG, "no intent found");
+                    Toast.makeText(getApplicationContext(), "no intent found", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         btn5.setOnClickListener(new View.OnClickListener(){
@@ -73,5 +100,23 @@ public class ClipboardActivity extends AppCompatActivity {
 
             }
         });
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(TEST_BROADCAST);
+        this.registerReceiver( mReceiver, intentFilter);
+    }
+
+    BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "action : "+ intent.toString());
+            Toast.makeText(getApplicationContext(), "action : "+ intent.toString(), Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
     }
 }
