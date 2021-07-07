06e7f1e59e85cd8768821b1edcf7935652306008
jayce <dontbeshy8888@gmail.com>
2021-07-08 00:19:02 +0900

    add test for customview circle

--- Fragment/app/src/main/java/com/toy/fragment/ui/main/MyClockFragment.java
+++ Fragment/app/src/main/java/com/toy/fragment/ui/main/MyClockFragment.java
@@ -5,18 +5,24 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.toy.fragment.R;
+import com.toy.fragment.customview.SuperCircleView;
 
 import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link MyClockFragment#newInstance} factory method to
@@ -28,6 +34,7 @@ public class MyClockFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private SuperCircleView mSuperCircleView;
 
 
     public static MyClockFragment newInstance() {
@@ -86,21 +93,55 @@ public class MyClockFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_clock, container, false);
+        View myClockFragment = inflater.inflate(R.layout.fragment_my_clock, container, false);
+
+        LinearLayout googleLogInButton = myClockFragment.findViewById(R.id.google_login);
+        googleLogInButton.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(myClockFragment.getContext(),"google login clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+        LinearLayout kakaoLogInButton = myClockFragment.findViewById(R.id.kakao_login_button);
+        kakaoLogInButton.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(myClockFragment.getContext(),"kakao login clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        TextView circleText = myClockFragment.findViewById(R.id.circle_text);
+        mSuperCircleView = myClockFragment.findViewById(R.id.superview);
+
+        mSuperCircleView.setValue(100,circleText);
+
+        mSuperCircleView.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //Randomly set the ring size
+                int i = new Random().nextInt(100) + 1;
+                Log.i("test","onClick : i = " + i);
+                mSuperCircleView.setValue(i,circleText);
+            }
+        });
+
+
+        return myClockFragment;
     }
 
     @Override
     public void onResume() {
         super.onResume();
         setCurrentTime();
-        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+       // getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         //getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
 
     }
 
     @Override
     public void onPause() {
-        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onPause();
     }
 }
